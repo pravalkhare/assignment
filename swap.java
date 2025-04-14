d5f4df430ef2c34d4c04610cf1927fe41b9f6c8f
pravalkhare <pravalkhare24@gmail.com>
2025-04-14 22:25:44 +0530

    Update swap.java

--- swap.java
+++ swap.java
@@ -5,11 +5,11 @@ class Swap
     {   
         System.out.println("Enter the value of x and y");  
         Scanner sc = new Scanner(System.in);  
-        /Define variables/  
+          
         int x = sc.nextInt();  
         int y = sc.nextInt();  
         System.out.println("before swapping numbers: "+x +" "+ y);  
-       /Swapping/  
+         
         x = x + y;   
         y = x - y;   
         x = x - y;   
